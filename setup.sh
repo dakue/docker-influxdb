1962c2e4e6136b373a18a48b489b796b854ec444
Daniel Kühne <daniel.kuehne@unbelievable-machine.com>
2015-09-01 16:11:18 +0200

    add script to create the metrics database und users

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+API_URL="http://localhost:8086"
+
+echo "INFO: starting InfluxDB setup script"
+
+#wait for the startup of influxdb
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "INFO: Waiting for confirmation of InfluxDB service startup ..."
+    sleep 3 
+    curl -k ${API_URL}/ping 2> /dev/null
+    RET=$?
+done
+echo ""
+
+if [ -z "$INFLUXDB_USERNAME" ]
+then
+    INFLUXDB_USERNAME="root"
+fi
+
+if [ -z "$INFLUXDB_PASSWORD" ]
+then
+    INFLUXDB_PASSWORD="root"
+fi
+
+echo "INFO: Creating user $INFLUXDB_USERNAME with password"
+/opt/influxdb/influx -host=localhost -execute "CREATE USER $INFLUXDB_USERNAME WITH PASSWORD '$INFLUXDB_PASSWORD' WITH ALL PRIVILEGES"
+
+if [ -n "${PRE_CREATE_DB}" ]
+then
+    echo "INFO: About to create the following database: ${PRE_CREATE_DB}"
+    if [ -f "/data/.pre_db_created" ]
+    then
+        echo "INFO: Database had been created before, skipping ..."
+    else
+        arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
+
+        for x in $arr
+        do
+            echo "INFO: Creating database: ${x}"
+            /opt/influxdb/influx -host=localhost -port=8086 -username="$INFLUXDB_USERNAME" -password="$INFLUXDB_PASSWORD" -execute="CREATE DATABASE ${x}"
+        done
+        echo ""
+
+        touch "/data/.pre_db_created"
+        fg
+        exit 0
+    fi
+else
+    echo "INFO: No database needs to be pre-created"
+fi
+
+echo "INFO: all setup tasks are finished"
