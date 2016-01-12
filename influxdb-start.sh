fcb64653df389e0ab270c3d664525850b460df12
Daniel Kühne <dkhmailto@googlemail.com>
2016-01-12 20:56:29 +0100

    modify docker-entrypoint.sh to only start InfluxDB and rename it to influxdb-start.sh

--- influxdb-start.sh
+++ influxdb-start.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-set -e
-
-if [ "$1" = 'influxdb' ]
-then
-    # Pre create database on the initiation of the container
-    CONFIG_FILE="/etc/influxdb/config.toml"
-    PID_FILE="/var/run/influxdb/influxdb.pid"
-
-    mkdir -p /var/lib/influxdb/meta
-    mkdir -p /var/lib/influxdb/data
-    mkdir -p /var/lib/influxdb/hh
-    chown -R influxdb:influxdb /var/lib/influxdb
-
-    #/setup.sh &
     
-    echo "INFO: Executing envplate to replace environment variables in configuration file"
-    /usr/local/bin/ep -v "${CONFIG_FILE}"
+# Pre create database on the initiation of the container
+CONFIG_FILE="/etc/influxdb/config.toml"
+PID_FILE="/var/run/influxdb/influxdb.pid"
 
-    echo "INFO: Starting InfluxDB ..."
-    exec /usr/local/bin/gosu influxdb:influxdb /usr/bin/influxd -config "${CONFIG_FILE}" -pidfile "${PID_FILE}"
-fi
+mkdir -p /var/lib/influxdb/meta
+mkdir -p /var/lib/influxdb/data
+mkdir -p /var/lib/influxdb/hh
+chown -R influxdb:influxdb /var/lib/influxdb
 
-exec "$@"
+echo "INFO: Executing envplate to replace environment variables in configuration file"
+/usr/local/bin/ep -v "${CONFIG_FILE}"
+
+echo "INFO: Starting InfluxDB ..."
+exec /usr/local/bin/gosu influxdb:influxdb /usr/bin/influxd -config "${CONFIG_FILE}" -pidfile "${PID_FILE}"
